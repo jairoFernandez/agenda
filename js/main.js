5e6ef41ef7d9a8934cd0a2e2b91ee4cb698a8bc2
Jairo Fernandez <jairofdezvega@gmail.com>
2014-12-25 23:52:11 -0500

    ajustado main

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
+/**
+* Sistema de logs
+*/
+var i_log = 0;
+function mkLog(text){
+	var date = new Date();
+	var txt = i_log + " - " + date.getHours() + " - " + date.getMinutes() + " - " + date.getSeconds() + ": " + text;
+	i_log++;
+	console.log(txt);
+}
+
 function onBodyLoad(){
 	document.addEventListener("deviceready", onDeviceReady, false);
 }
 
 function onDeviceReady(){
-	navigator.notification.alert("PhoneGap is working");
+	mkLog("Aplicacion cargada y lista");
+	//navigator.notification.alert("PhoneGap is working");
 }
